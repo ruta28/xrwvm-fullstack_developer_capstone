350b100d328ae1507ba1d96d1362f86b07497a2f
ruta28 <rutalole28@gmail.com>
2026-03-17 11:55:01 -0400

    fix final flake8 lint errors

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -19,13 +19,25 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
-    "rutalole23-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
-    "rutalole23-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+    (
+        "rutalole23-8000.theianext-0-labs-prod-"
+        "misc-tools-us-east-0.proxy.cognitiveclass.ai"
+    ),
+    (
+        "rutalole23-8000.theiadockernext-1-labs-prod-"
+        "theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    ),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://rutalole23-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai",
-    "https://rutalole23-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai",
+    (
+        "https://rutalole23-8000.theianext-0-labs-prod-"
+        "misc-tools-us-east-0.proxy.cognitiveclass.ai"
+    ),
+    (
+        "https://rutalole23-8000.theiadockernext-1-labs-prod-"
+        "theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    ),
 ]
 
 REST_FRAMEWORK = {
